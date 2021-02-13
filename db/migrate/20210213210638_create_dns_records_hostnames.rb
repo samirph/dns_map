0eb4b9d6888ff45ec12e7b27174c6878aaceb905
Samir <samirphaddad@gmail.com>
2021-02-13 18:13:57 -0300

    Add DnsRecord and Hostname models

---
+++ db/migrate/20210213210638_create_dns_records_hostnames.rb
@@ -0,0 +1,8 @@
+class CreateDnsRecordsHostnames < ActiveRecord::Migration[6.1]
+  def change
+    create_join_table :dns_records, :hostnames do |t|
+      t.index :dns_record_id
+      t.index :hostname_id
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,37 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_02_13_210638) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "dns_records", force: :cascade do |t|
+    t.string "ipv4"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "dns_records_hostnames", id: false, force: :cascade do |t|
+    t.bigint "dns_record_id", null: false
+    t.bigint "hostname_id", null: false
+    t.index ["dns_record_id"], name: "index_dns_records_hostnames_on_dns_record_id"
+    t.index ["hostname_id"], name: "index_dns_records_hostnames_on_hostname_id"
+  end
+
+  create_table "hostnames", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
