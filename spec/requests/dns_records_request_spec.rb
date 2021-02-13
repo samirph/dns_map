5f8f1e5b26c5d1563536ac177f1dbcb1e224c035
Samir <samirphaddad@gmail.com>
2021-02-13 19:45:10 -0300

    Wrap create action specs in #create context

--- spec/requests/dns_records_request_spec.rb
+++ spec/requests/dns_records_request_spec.rb
@@ -3,19 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe 'DnsRecords', type: :request do
-  it 'returns status created with correct params' do
-    expect do
-      post '/dns-records', params: { ip: '1.1.1.1', hostnames_attributes: [{ hostname: 'domain.com' }] }
-    end.to change { DnsRecord.count }.by(1).and change { Hostname.count }.by(1)
+  context '#create' do
+    it 'returns status created with correct params' do
+      expect do
+        post '/dns-records', params: { ip: '1.1.1.1', hostnames_attributes: [{ hostname: 'domain.com' }] }
+      end.to change { DnsRecord.count }.by(1).and change { Hostname.count }.by(1)
 
-    expect(response).to have_http_status(:created)
-    expect(response.parsed_body['id']).to be_present
-  end
+      expect(response).to have_http_status(:created)
+      expect(response.parsed_body['id']).to be_present
+    end
 
-  it 'returns status unprocessible entity when missing the required params' do
-    post '/dns-records', params: {}
+    it 'returns status unprocessible entity when missing the required params' do
+      post '/dns-records', params: {}
 
-    expect(response).to have_http_status(:unprocessable_entity)
-    expect(response.parsed_body['id']).not_to be_present
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response.parsed_body['id']).not_to be_present
+    end
   end
 end
